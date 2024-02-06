c2ebbcb611ef9037597e5c1414c9c254d2b45c15
ahmads1990 <ahmads20002000@gmail.com>
2024-02-06 22:00:27 +0200

    Add testing project using Nunit

---
+++ VendingMachineApi.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace VendingMachineApi.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
